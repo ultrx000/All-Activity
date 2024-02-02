ab8737eb004c145cd8299fcd0cc6b5640b8918f2
ultr4op@gmail.com <egor.govorushko@net.ik>
2024-02-02 11:33:45 +0300

    Добавлены восклицательные знаки!

--- src/third.java
+++ src/third.java
@@ -5,16 +5,16 @@ public class third {
         Date time = new Date();
         int hours = time.getHours();
         if (hours >= 6 && hours <= 12) {
-            System.out.println("Good morning! It's " + hours + " o'clock now");
+            System.out.println("Good morning! It's " + hours + " o'clock now!");
         }
         else if (hours >= 13 && hours <= 15) {
-            System.out.println("Good Day! It's " + hours + " o'clock now");
+            System.out.println("Good Day! It's " + hours + " o'clock now!");
         }
         else if (hours >= 16) {
-            System.out.println("Good Evening! It's " + hours + " o'clock now");
+            System.out.println("Good Evening! It's " + hours + " o'clock now!");
         }
         else if (hours >= 1) {
-            System.out.println("Good Night! It's " + hours + " o'clock now");
+            System.out.println("Good Night! It's " + hours + " o'clock now!");
         }
     }
 }
